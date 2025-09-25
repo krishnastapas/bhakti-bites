cab8995be10b4999a2915f09cd64a3775e1d7192
Tapas Manna <74945920+krishnastapas@users.noreply.github.com>
2025-09-25 19:49:24 +0530

    SEO updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,38 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// export const metadata: Metadata = {
+//   title: "Bhakti Bites",
+//   description: "Mahaprasad made by ISKCON Devotees",
+// };
+
+// import type { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: "Bhakti Bites",
-  description: "Mahaprasad made by ISKCON Devotees",
+  title: "Bhakti Bites - Satvik Snacks",
+  description: "Healthy Mahaprasad cookies made with jaggery and wheat flour. Made by ISKCON Devotees",
+  keywords: ["Satvik snacks", "gur cookies", "healthy sweets","Mahaprasad Cookies"],
+  openGraph: {
+    title: "Bhakti Bites",
+    description: "Delicious Satvik cookies and sweets.",
+    url: "https://bhaktibites.com",
+    siteName: "Bhakti Bites",
+    images: [
+      {
+        url: "/images/banner1.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bhakti Bites",
+    description: "Healthy Satvik snacks delivered fresh.",
+    images: ["/images/banner1.png"],
+  },
 };
 
 export default function RootLayout({
